edc792ab1079392a5dde77f098a220ff220e95bb
kamizane <julioitabira@hotmail.com>
2024-11-12 09:44:26 -0300

    correcoes da lista sonda e sonda espaical

--- Lista_sonda_espacial.c
+++ Lista_sonda_espacial.c
@@ -1,5 +1,6 @@
 #include "Lista_sonda_espacial.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void inicializa_lista_sonda_espacial(Lista_sonda_espacial * lista_sonda){
     lista_sonda->pPrimeiro = (Celula*) malloc (sizeof(Celula));
@@ -30,7 +31,7 @@ int retira_item_lista_sonda_espacial(Lista_sonda_espacial* lista_sonda, char * i
         int count = 1;
         //ércorre a lista até encontrar o ultimo item
         while (pAux->pProx != NULL){
-            if(pAux->item_sonda.Identificador == *id){
+            if(*pAux->item_sonda.Identificador == *id){
                 if (count == lista_sonda->QntItens){
                     *pitem = pAux->item_sonda;
                     pAux_ant->pProx = NULL;
@@ -56,6 +57,17 @@ int retira_item_lista_sonda_espacial(Lista_sonda_espacial* lista_sonda, char * i
         return 0;
     }
 }
-void imprime_lista_sonda_espacial(Lista_sonda_espacial* lista_sonda){
-    
+int imprime_lista_sonda_espacial(Lista_sonda_espacial* lista_sonda){
+    if (verifica_lista_vazia(lista_sonda)){
+        printf("Lista vazia\n");
+        return 0;
+    }
+    else {
+        Celula* pAux = lista_sonda->pPrimeiro->pProx;
+        while(pAux->pProx!= NULL){
+            printf("%s, ", pAux->item_sonda.Identificador);
+            pAux = pAux->pProx;
+        }
+    }
+    return 1;
 }
